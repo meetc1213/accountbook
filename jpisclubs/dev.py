21435cfa9f6e4ebe90e727e1ec44f14ec47a1f8f
Meet Chaudhari <63335174+meetc1213@users.noreply.github.com>
2020-09-22 16:36:11 +0530

    hello

--- jpisclubs/dev.py
+++ jpisclubs/dev.py
@@ -4,6 +4,7 @@ SECRET_KEY = '@(g2e0_lg1i@3y1&u57hw)bzr#vnh1!-_j5x-vr7)%+o*%^c)%'
 ALLOWED_HOSTS = ['*']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -11,3 +12,4 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
